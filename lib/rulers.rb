b411141cd9ed58e26d5e76d71f6aa094be9139b6
Miriam Williams <miriam.williams@hitcents.com>
2013-11-25 13:50:58 -0600

    Cut and paste from pdf to find error

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,25 +1,22 @@
-require "rulers/array"
 require "rulers/version"
 require "rulers/routing"
 
 module Rulers
-  class Application
-    def call(env)
-      klass, act = get_controller_and_action(env)
-      controller = klass.new(env)
-      text = controller.send(act)
-      [200, {'Content-Type' => 'text/html'},
-        [text]]
-    end
-  end
-
-  class Controller
-    def initialize(env)
-      @env = env
-    end
-
-    def env
-      @env
-    end
-  end
+  class Application
+    def call(env)
+      klass, act = get_controller_and_action(env)
+      controller = klass.new(env)
+      text = controller.send(act)
+      [200, {'Content-Type' => 'text/html'},
+[text]]
+    end
+  end
+  class Controller
+    def initialize(env)
+      @env = env
+    end
+    def env
+      @env
+    end
+  end
 end
