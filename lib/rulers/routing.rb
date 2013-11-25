42cd6607569c0964f5005b133067e7a8bd0a4095
Miriam Williams <miriam.williams@hitcents.com>
2013-11-25 12:16:54 -0600

    Fix another typo to fix NameError

--- lib/rulers/routing.rb
+++ lib/rulers/routing.rb
@@ -1,6 +1,6 @@
 module Rulers
   class Application
-    def get_controller_and_actino(env)
+    def get_controller_and_action(env)
       _, cont, action, after =
         env["PATH_INFO"].split('/', 4)
       cont = cont.capitalize #"People"
