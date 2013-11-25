f9d3a8c0f4463aa860b4f3fd5a61dd3323ec4096
Miriam Williams <miriam.williams@hitcents.com>
2013-11-25 09:53:59 -0600

    Add more methods to array.rb

--- lib/rulers/array.rb
+++ lib/rulers/array.rb
@@ -1,5 +1,20 @@
 class Array
   def sum(start = 0)
-    inject(sart, &:+)
+    inject(start, &:+)
   end
+
+  def diff(start = 0)
+    inject(start, &:-)
+  end
+
+  def product(start = 0)
+    inject(start, &:*)
+  end
+
+  def quotient(start = 0)
+    inject(start, &:/)
+  end
+
+
 end
+
